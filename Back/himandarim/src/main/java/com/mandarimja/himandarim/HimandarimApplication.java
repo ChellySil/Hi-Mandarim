676e74c7d1c19d16012c1c90b944715ba487d8eb
Michelle <silvamichelle1417@gmail.com>
2024-07-03 23:40:07 -0300

    começando back

---
+++ Back/himandarim/src/main/java/com/mandarimja/himandarim/HimandarimApplication.java
@@ -0,0 +1,13 @@
+package com.mandarimja.himandarim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimandarimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimandarimApplication.class, args);
+	}
+
+}
---
+++ Back/himandarim/src/test/java/com/mandarimja/himandarim/HimandarimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandarimja.himandarim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimandarimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
